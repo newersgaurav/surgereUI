fac8350a8c734096353098c7f5bb866fb4e8d48b
gauravapandey <gaurav.pandey@tothenew.com>
2019-10-11 15:39:21 +0530

    finishing is commiting

--- assests/js/custom.js
+++ assests/js/custom.js
@@ -31,3 +31,38 @@ $(".password-field .eye-icon").click(function (){
         $(".password-field input").attr("type","password");
     }
 })
+
+function increase(id){
+    console.log("Entry 1");
+    var count;
+    var val = $('#' + id + ' .' + id + '-value').attr("value");
+    console.log(val);
+    if( val === "Height" || val === "Width"){
+        count = 0;
+        console.log("Entry 2");
+    }
+    else{
+        count = val;
+        console.log("Entry 2");
+    }
+    count = parseInt(count) + 1;
+    $('#' + id + ' .' + id + '-value').attr("value", count);
+    $('#' + id + ' .' + id + '-value').html(count);
+}
+
+function decrease(id){
+    var count;
+    var val = $('#' + id + ' .' + id + '-value').attr("value");
+    if( val === "Height" || val === "Width"){
+        count = 0;
+    }
+    else{
+        count = val;
+    }
+    count = parseInt(count) - 1;
+    if(count < 0){
+        count = 0;
+    }
+    $('#' + id + ' .' + id + '-value').attr("value", count);
+    $('#' + id + ' .' + id + '-value').html(count);
+}
